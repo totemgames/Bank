c524ded0c732177169542fea6f9dcc2afbee665f
Artem <1ttem12@gmail.com>
2017-02-28 23:32:50 +0100

    Added class Transaction

---
+++ src/main/java/com/ttem/model/transaction/Transaction.java
@@ -0,0 +1,46 @@
+package com.ttem.model.transaction;
+
+import java.util.Date;
+
+public abstract class Transaction {
+
+    private final double amount;
+    private final String description;
+    private Date date;
+    private boolean done;
+
+    protected Transaction(final double amount, final String description) {
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public abstract boolean doTransaction();
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isDone() {
+        return this.done;
+    }
+
+    protected void setDate(final Date date) {
+        if (this.getDate() == null) {
+            this.date = date;
+        }
+    }
+
+    protected void setDone(final boolean done) {
+        if (!this.isDone()) {
+            this.done = done;
+        }
+    }
+}

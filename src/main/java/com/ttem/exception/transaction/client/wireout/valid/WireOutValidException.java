3e2ba4c5c44ddfdfff58f24b288ba0a7fc3f6a4b
Artem <1ttem12@gmail.com>
2017-03-01 00:28:34 +0100

    Added WireOutValidException extends WireOutException

---
+++ src/main/java/com/ttem/exception/transaction/client/wireout/valid/WireOutValidException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.transaction.client.wireout.valid;
+
+import com.ttem.exception.transaction.client.wireout.WireOutException;
+
+public class WireOutValidException extends WireOutException {
+
+    public WireOutValidException() {
+    }
+
+    public WireOutValidException(final String message) {
+        super(message);
+    }
+}

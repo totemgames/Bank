5bc6b4788387df5e3abaf7743a1893b53909eeb8
Artem <1ttem12@gmail.com>
2017-02-28 23:55:25 +0100

    Account added historyTransaction and methods: -private List<AccountTransaction> getHistoryTransactionFromDataBase()

--- src/main/java/com/ttem/model/account/Account.java
+++ src/main/java/com/ttem/model/account/Account.java
@@ -1,12 +1,23 @@
 package com.ttem.model.account;
 
+import com.ttem.model.transaction.account.AccountTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
 
     private final byte[] number;
+    private final List<AccountTransaction> historyTransaction;
     private final String description;
 
     public Account(final byte[] number, final String description) {
         this.number = number;
+        this.historyTransaction = getHistoryTransactionFromDataBase();
         this.description = description;
+    }
+
+    private List<AccountTransaction> getHistoryTransactionFromDataBase() {
+        return new ArrayList<>();
     }
 }

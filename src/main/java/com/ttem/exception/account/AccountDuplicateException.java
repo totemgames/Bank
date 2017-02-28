48f6e34b431cd6bf482fa3724374654f06d2a0b3
Artem <1ttem12@gmail.com>
2017-02-28 23:48:07 +0100

    Added AccountDuplicateException

---
+++ src/main/java/com/ttem/exception/account/AccountDuplicateException.java
@@ -0,0 +1,11 @@
+package com.ttem.exception.account;
+
+public class AccountDuplicateException extends AccountException{
+
+    public AccountDuplicateException() {
+    }
+
+    public AccountDuplicateException(final String message) {
+        super(message);
+    }
+}

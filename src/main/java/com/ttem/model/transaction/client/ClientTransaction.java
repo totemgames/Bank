f872cb0b59cae642bc60a5050bb5961ec726264f
Artem <1ttem12@gmail.com>
2017-02-28 23:34:15 +0100

    Added class ClientTransaction

---
+++ src/main/java/com/ttem/model/transaction/client/ClientTransaction.java
@@ -0,0 +1,13 @@
+package com.ttem.model.transaction.client;
+
+import com.ttem.model.transaction.Transaction;
+
+public abstract class ClientTransaction extends Transaction{
+
+    public ClientTransaction(final double amount, final String description) {
+        super(amount, description);
+    }
+
+    @Override
+    public abstract boolean doTransaction();
+}

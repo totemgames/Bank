e3b2b74c5f613cc6ea5db6c7c5fdf3b3ab772959
Artem <1ttem12@gmail.com>
2017-02-28 23:39:01 +0100

    WireOut add historyTransaction and account list and methods: -private List<ClientTransaction> getHistoryTransactionFromDataBase() -private List<Account> getAccountListFromDataBase()

--- src/main/java/com/ttem/model/account/Client.java
+++ src/main/java/com/ttem/model/account/Client.java
@@ -1,8 +1,26 @@
 package com.ttem.model.account;
 
+import com.ttem.model.transaction.client.ClientTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client extends Person {
+
+    private final List<ClientTransaction> historyTransaction;
+    private final List<Account> accountList;
 
     public Client(final String firstName, final String lastName) {
         super(firstName, lastName);
+        this.historyTransaction = getHistoryTransactionFromDataBase();
+        this.accountList = getAccountListFromDataBase();
+    }
+
+    private List<ClientTransaction> getHistoryTransactionFromDataBase() {
+        return new ArrayList<>();
+    }
+
+    private List<Account> getAccountListFromDataBase() {
+        return new ArrayList<>();
     }
 }

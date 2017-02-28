e9e2fa066a3fd043b1f0296610e4421f1184c9f1
Artem <1ttem12@gmail.com>
2017-03-01 00:06:00 +0100

    Added TransactionException extends BankException

---
+++ src/main/java/com/ttem/exception/transaction/TransactionException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.transaction;
+
+import com.ttem.exception.bank.BankException;
+
+public class TransactionException extends BankException{
+
+    public TransactionException() {
+    }
+
+    public TransactionException(final String message) {
+        super(message);
+    }
+}

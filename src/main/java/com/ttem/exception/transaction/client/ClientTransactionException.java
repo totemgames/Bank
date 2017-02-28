1125100248299986dba42b23cd1ab33d57d16da4
Artem <1ttem12@gmail.com>
2017-03-01 00:08:12 +0100

    Added ClientTransactionException extends TransactionException

---
+++ src/main/java/com/ttem/exception/transaction/client/ClientTransactionException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.transaction.client;
+
+import com.ttem.exception.transaction.TransactionException;
+
+public class ClientTransactionException extends TransactionException{
+
+    public ClientTransactionException() {
+    }
+
+    public ClientTransactionException(final String message) {
+        super(message);
+    }
+}

4b7f3769309351168c84b8019064124c58138796
Artem <1ttem12@gmail.com>
2017-03-01 00:26:54 +0100

    Added WireOutException extends ClientTransactionException

---
+++ src/main/java/com/ttem/exception/transaction/client/wireout/WireOutException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.transaction.client.wireout;
+
+import com.ttem.exception.transaction.client.ClientTransactionException;
+
+public class WireOutException extends ClientTransactionException{
+
+    public WireOutException() {
+    }
+
+    public WireOutException(final String message) {
+        super(message);
+    }
+}

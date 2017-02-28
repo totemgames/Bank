5e57a13a7cced9b556ae3b948ffaa09b96e409c8
Artem <1ttem12@gmail.com>
2017-03-01 00:16:36 +0100

    Added AccountTransactionException extends TransactionException

---
+++ src/main/java/com/ttem/exception/transaction/account/AccountTransactionException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.transaction.account;
+
+import com.ttem.exception.transaction.TransactionException;
+
+public class AccountTransactionException extends TransactionException{
+
+    public AccountTransactionException() {
+    }
+
+    public AccountTransactionException(final String message) {
+        super(message);
+    }
+}

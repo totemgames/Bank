477c18fa7f71183c662d5f9186a417cd6891be88
Artem <1ttem12@gmail.com>
2017-03-01 14:35:53 +0100

    Added CheckDoneException extends CheckValidException

---
+++ src/main/java/com/ttem/exception/transaction/account/check/valid/CheckDoneException.java
@@ -0,0 +1,11 @@
+package com.ttem.exception.transaction.account.check.valid;
+
+public class CheckDoneException extends CheckValidException{
+
+    public CheckDoneException() {
+    }
+
+    public CheckDoneException(final String message) {
+        super(message);
+    }
+}

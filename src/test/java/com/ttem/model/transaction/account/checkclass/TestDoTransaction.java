4afb6efe8cac0a0e0d5185cce7eec56aca19570e
Artem <1ttem12@gmail.com>
2017-03-01 16:01:05 +0100

    checkclass.TestDoTransaction added tests: -public void whenInputCorrectCheck() -@Test(expected = CheckAmountException.class)      public void whenInputIncorrectCheckAmount()  -@Test(expected = CheckAccountException.class)       public void whenInputIncorrectCheckAccountNumber() -public void whenInputIncorrectCheckAccountNull()

--- src/test/java/com/ttem/model/transaction/account/checkclass/TestDoTransaction.java
+++ src/test/java/com/ttem/model/transaction/account/checkclass/TestDoTransaction.java
@@ -1,8 +1,13 @@
 package com.ttem.model.transaction.account.checkclass;
 
+import com.ttem.exception.transaction.TransactionException;
+import com.ttem.exception.transaction.account.check.valid.CheckAccountException;
+import com.ttem.exception.transaction.account.check.valid.CheckAmountException;
 import com.ttem.model.account.Account;
 import com.ttem.model.transaction.account.Check;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestDoTransaction {
 
@@ -23,5 +28,25 @@ public class TestDoTransaction {
         incorrectCheckAmount = new Check(-3.4, correctAccount, "incorrect amount");
         incorrectCheckAccountNumber = new Check(3.4, incorrectAccountNumber, "incorrect account number");
         incorrectCheckAccountNull = new Check(3.4, null, "incorrect account null");
+    }
+
+    @Test
+    public void whenInputCorrectCheck() throws TransactionException {
+        Assert.assertTrue(correctCheck.doTransaction());
+    }
+
+    @Test(expected = CheckAmountException.class)
+    public void whenInputIncorrectCheckAmount() throws TransactionException {
+        incorrectCheckAmount.doTransaction();
+    }
+
+    @Test(expected = CheckAccountException.class)
+    public void whenInputIncorrectCheckAccountNumber() throws TransactionException {
+        incorrectCheckAccountNumber.doTransaction();
+    }
+
+    @Test
+    public void whenInputIncorrectCheckAccountNull() throws TransactionException {
+        Assert.assertFalse(incorrectCheckAccountNull.doTransaction());
     }
 }

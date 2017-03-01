04c31476e5ff928a339427cfe7cfb321cb02c4ce
Artem <1ttem12@gmail.com>
2017-03-01 14:59:56 +0100

    ConsoleView added methods: -public void printClient(final Client client) -public void printAccount(final Account account)

--- src/main/java/com/ttem/view/console/ConsoleView.java
+++ src/main/java/com/ttem/view/console/ConsoleView.java
@@ -1,7 +1,10 @@
 package com.ttem.view.console;
 
+import com.ttem.model.account.Account;
 import com.ttem.model.account.Client;
 import com.ttem.model.bank.Bank;
+import com.ttem.model.transaction.account.AccountTransaction;
+import com.ttem.model.transaction.client.ClientTransaction;
 import com.ttem.view.DefaultView;
 
 public class ConsoleView implements DefaultView {
@@ -12,6 +15,24 @@ public class ConsoleView implements DefaultView {
         System.out.println("Clients list:");
         for (Client c : bank.getClientDataBase()) {
             System.out.println(c.getFirstName());
+        }
+        System.out.println();
+    }
+
+    public void printClient(final Client client){
+        System.out.println("Client: " + client.getFirstName());
+        System.out.println("Client transactions");
+        for (ClientTransaction clientTransaction : client.getHistoryTransaction()){
+            System.out.println(clientTransaction);
+        }
+        System.out.println();
+    }
+
+    public void printAccount(final Account account){
+        System.out.println("Account: " + account);
+        System.out.println("Client transactions");
+        for (AccountTransaction accountTransaction : account.getHistoryTransaction()){
+            System.out.println(accountTransaction);
         }
         System.out.println();
     }

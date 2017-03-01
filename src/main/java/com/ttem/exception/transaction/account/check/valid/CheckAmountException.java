30b8418c7470970f1b64aa2473f21f8c13a738d0
Artem <1ttem12@gmail.com>
2017-03-01 14:37:38 +0100

    Added CheckAmountException extends CheckValidException

---
+++ src/main/java/com/ttem/exception/transaction/account/check/valid/CheckAmountException.java
@@ -0,0 +1,11 @@
+package com.ttem.exception.transaction.account.check.valid;
+
+public class CheckAmountException extends CheckValidException{
+
+    public CheckAmountException() {
+    }
+
+    public CheckAmountException(final String message) {
+        super(message);
+    }
+}

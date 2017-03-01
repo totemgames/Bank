f9f94e6841a43ecc26fbf379e1e217d0cd346194
Artem <1ttem12@gmail.com>
2017-03-01 11:55:59 +0100

    Added class Bank with method private List<Client> getClientListFromDataBase()

---
+++ src/main/java/com/ttem/model/bank/Bank.java
@@ -0,0 +1,20 @@
+package com.ttem.model.bank;
+
+import com.ttem.model.account.Client;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+
+    private final String name;
+    private final List<Client> clientDataBase;
+
+    public Bank(final String name) {
+        this.name = name;
+        this.clientDataBase = getClientListFromDataBase();
+    }
+
+    private List<Client> getClientListFromDataBase() {
+        return new ArrayList<>();
+    }
+}

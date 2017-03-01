f149b9fe241bc28101759d52a6899700842fb622
Artem <1ttem12@gmail.com>
2017-03-01 11:59:07 +0100

    Added ClientException extends BankException

---
+++ src/main/java/com/ttem/exception/account/client/ClientException.java
@@ -0,0 +1,13 @@
+package com.ttem.exception.account.client;
+
+import com.ttem.exception.bank.BankException;
+
+public class ClientException extends BankException{
+
+    public ClientException() {
+    }
+
+    public ClientException(final String message) {
+        super(message);
+    }
+}

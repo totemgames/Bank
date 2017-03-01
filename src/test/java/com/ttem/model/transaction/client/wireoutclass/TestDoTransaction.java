a7cb7c75c718963f87cf7c29c6de8132332ee2d2
Artem <1ttem12@gmail.com>
2017-03-01 16:08:11 +0100

    wireoutclass.TestDoTransaction added setUp

--- src/test/java/com/ttem/model/transaction/client/wireoutclass/TestDoTransaction.java
+++ src/test/java/com/ttem/model/transaction/client/wireoutclass/TestDoTransaction.java
@@ -1,4 +1,65 @@
 package com.ttem.model.transaction.client.wireoutclass;
 
+import com.ttem.model.account.Account;
+import com.ttem.model.transaction.client.WireOut;
+import org.junit.BeforeClass;
+
 public class TestDoTransaction {
+
+    private static Account correctAccount;
+    private static Account incorrectAccountNumber;
+
+    private static WireOut correctWireOut;
+    private static WireOut incorrectWireOutAmount;
+    private static WireOut incorrectWireOutSwift;
+    private static WireOut incorrectWireOutAccountNumber;
+    private static WireOut incorrectWireOutAccountNull;
+
+    @BeforeClass
+    public static void setUp() {
+        correctAccount = new Account(new byte[15], "correct");
+        incorrectAccountNumber = new Account(new byte[12], "incorrect number");
+
+        correctWireOut = new WireOut.Builder()
+                .setAmount(23.3)
+                .setToAccount(correctAccount)
+                .setSwift(new byte[15])
+                .setCountry("USA")
+                .setDescription("from Poland")
+                .build();
+
+        incorrectWireOutAmount = new WireOut.Builder()
+                .setAmount(-11.4)
+                .setToAccount(correctAccount)
+                .setSwift(new byte[15])
+                .setCountry("USA")
+                .setDescription(" from Poland")
+                .build();
+
+        incorrectWireOutSwift = new WireOut.Builder()
+                .setAmount(3.2)
+                .setToAccount(correctAccount)
+                .setSwift(new byte[12])
+                .setCountry("USA")
+                .setDescription("from Poland")
+                .build();
+
+
+        incorrectWireOutAccountNumber = new WireOut.Builder()
+                .setAmount(3.2)
+                .setToAccount(incorrectAccountNumber)
+                .setSwift(new byte[15])
+                .setCountry("USA")
+                .setDescription("from Poland")
+                .build();
+
+
+        incorrectWireOutAccountNull = new WireOut.Builder()
+                .setAmount(3.2)
+                .setToAccount(null)
+                .setSwift(new byte[15])
+                .setCountry("USA")
+                .setDescription("from Poland")
+                .build();
+    }
 }

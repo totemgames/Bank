b199e1e068a053e185a2cd02c6cc94bc355be013
Artem <1ttem12@gmail.com>
2017-03-01 15:37:15 +0100

    clientclass.TestAddNewAccount added setUp

--- src/test/java/com/ttem/model/account/clientclass/TestAddNewAccount.java
+++ src/test/java/com/ttem/model/account/clientclass/TestAddNewAccount.java
@@ -1,4 +1,23 @@
 package com.ttem.model.account.clientclass;
 
+import com.ttem.model.account.Account;
+import com.ttem.model.account.Client;
+import org.junit.BeforeClass;
+
 public class TestAddNewAccount {
+
+    private static Account correctAccount;
+    private static Account incorrectAccountNumber;
+    private static Account incorrectAccountNull;
+
+    private static Client correctClient;
+
+    @BeforeClass
+    public static void setUp() {
+        correctAccount = new Account(new byte[15], "correct");
+        incorrectAccountNumber = new Account(new byte[12], "incorrect number");
+        incorrectAccountNull = null;
+
+        correctClient = new Client("Bob", "Ross");
+    }
 }

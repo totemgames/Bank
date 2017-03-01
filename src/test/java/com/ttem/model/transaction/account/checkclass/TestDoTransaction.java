1457cd6e041ddb366dea671fd09c1cee0c3ccfe5
Artem <1ttem12@gmail.com>
2017-03-01 15:59:56 +0100

    checkclass.TestDoTransaction added setUp

--- src/test/java/com/ttem/model/transaction/account/checkclass/TestDoTransaction.java
+++ src/test/java/com/ttem/model/transaction/account/checkclass/TestDoTransaction.java
@@ -1,7 +1,27 @@
 package com.ttem.model.transaction.account.checkclass;
 
-/**
- * Created by 1ttem on 3/1/2017.
- */
+import com.ttem.model.account.Account;
+import com.ttem.model.transaction.account.Check;
+import org.junit.BeforeClass;
+
 public class TestDoTransaction {
+
+    private static Account correctAccount;
+    private static Account incorrectAccountNumber;
+
+    private static Check correctCheck;
+    private static Check incorrectCheckAmount;
+    private static Check incorrectCheckAccountNumber;
+    private static Check incorrectCheckAccountNull;
+
+    @BeforeClass
+    public static void setUp() {
+        correctAccount = new Account(new byte[15], "correct");
+        incorrectAccountNumber = new Account(new byte[12], "incorrect number");
+
+        correctCheck = new Check(2.3, correctAccount, "correct check");
+        incorrectCheckAmount = new Check(-3.4, correctAccount, "incorrect amount");
+        incorrectCheckAccountNumber = new Check(3.4, incorrectAccountNumber, "incorrect account number");
+        incorrectCheckAccountNull = new Check(3.4, null, "incorrect account null");
+    }
 }

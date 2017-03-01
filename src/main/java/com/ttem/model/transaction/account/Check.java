ba170eea4f205b18879a8af1ee70c217cf67bf81
Artem <1ttem12@gmail.com>
2017-03-01 14:31:46 +0100

    Check added get method and private boolean executionOfTransaction()

--- src/main/java/com/ttem/model/transaction/account/Check.java
+++ src/main/java/com/ttem/model/transaction/account/Check.java
@@ -3,6 +3,8 @@ package com.ttem.model.transaction.account;
 import com.ttem.exception.transaction.account.AccountTransactionException;
 import com.ttem.model.account.Account;
 
+import java.util.Date;
+
 public class Check extends AccountTransaction {
 
     private final Account toAccount;
@@ -15,5 +17,15 @@ public class Check extends AccountTransaction {
     @Override
     public boolean doTransaction() throws AccountTransactionException {
         return false;
+    }
+
+    public Account getToAccount() {
+        return this.toAccount;
+    }
+
+    private boolean executionOfTransaction() {
+        this.setDone(true);
+        this.setDate(new Date());
+        return this.isDone();
     }
 }

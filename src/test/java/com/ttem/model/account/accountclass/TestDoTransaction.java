cc732aeed5d108d9dbdd45b6b2d5b96e384c54af
Artem <1ttem12@gmail.com>
2017-03-01 15:30:38 +0100

    accountclass.TestDoTransaction added tests: -public void correctAccountWhenInputCorrectCheck() -public void correctAccountWhenInputCorrectDeposit()

--- src/test/java/com/ttem/model/account/accountclass/TestDoTransaction.java
+++ src/test/java/com/ttem/model/account/accountclass/TestDoTransaction.java
@@ -1,9 +1,13 @@
 package com.ttem.model.account.accountclass;
 
+import com.ttem.exception.account.AccountException;
+import com.ttem.exception.transaction.TransactionException;
 import com.ttem.model.account.Account;
 import com.ttem.model.transaction.account.Check;
 import com.ttem.model.transaction.account.Deposit;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestDoTransaction {
 
@@ -20,5 +24,15 @@ public class TestDoTransaction {
 
         correctCheck = new Check(2.3, correctAccount, "correct check");
         correctDeposit = new Deposit(2.3, correctAccount, "correct deposit");
+    }
+
+    @Test
+    public void correctAccountWhenInputCorrectCheck() throws AccountException, TransactionException {
+        Assert.assertTrue(correctAccount.doTransaction(correctCheck));
+    }
+
+    @Test
+    public void correctAccountWhenInputCorrectDeposit() throws AccountException, TransactionException {
+        Assert.assertTrue(correctAccount.doTransaction(correctDeposit));
     }
 }

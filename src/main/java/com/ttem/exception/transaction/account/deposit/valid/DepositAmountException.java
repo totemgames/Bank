645c4933c90f81efc9bf0a7b970ba963c294d0d3
Artem <1ttem12@gmail.com>
2017-03-01 14:51:56 +0100

    Added DepositAmountException extends DepositValidException

---
+++ src/main/java/com/ttem/exception/transaction/account/deposit/valid/DepositAmountException.java
@@ -0,0 +1,11 @@
+package com.ttem.exception.transaction.account.deposit.valid;
+
+public class DepositAmountException extends DepositValidException {
+
+    public DepositAmountException() {
+    }
+
+    public DepositAmountException(final String message) {
+        super(message);
+    }
+}

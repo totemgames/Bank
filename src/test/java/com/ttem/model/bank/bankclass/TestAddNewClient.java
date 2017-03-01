e75f874810d8b119f093d35063c60a3876b5fe85
Artem <1ttem12@gmail.com>
2017-03-01 15:45:35 +0100

    bankclass.TestAddNewClient added test public void whenInputCorrectClient()

--- src/test/java/com/ttem/model/bank/bankclass/TestAddNewClient.java
+++ src/test/java/com/ttem/model/bank/bankclass/TestAddNewClient.java
@@ -1,4 +1,22 @@
 package com.ttem.model.bank.bankclass;
 
+import com.ttem.model.account.Client;
+import com.ttem.model.bank.Bank;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestAddNewClient {
+
+    private static Bank bank = new Bank("ING");
+    private static Client correctClient = new Client("Bob", "Ross");;
+
+    @Test
+    public void whenInputCorrectClient() throws Exception {
+        Client expectedClient = correctClient;
+
+        Assert.assertTrue(bank.addNewClient(expectedClient));
+
+        Client actualClient = bank.getClientDataBase().get(0);
+        Assert.assertSame(expectedClient, actualClient);
+    }
 }
